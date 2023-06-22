e901548c393fe23896d80b085c7bcbccafe27c2c
alirafli <mrafli1212@gmail.com>
2023-06-22 10:56:22 +0700

    feat: add footer

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const studentName = "Mohammad Ali Rafli" // TODO: replace this
+const studentId = "FE4234561" // TODO: replace this
 
 export { url, studentName, studentId }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 // TODO: answer here
 
+import { Box } from "@chakra-ui/react";
+
 const Footer = () => {
-    return (
-        // TODO: answer here
-    );
+  return (
+    <Box className="footer">
+      <p className="studentName">Mohammad Ali Rafli</p>
+      <p className="studentId">FE4234561</p>
+    </Box>
+  );
 };
 
 export default Footer;
